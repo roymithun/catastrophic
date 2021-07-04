7a83e6f07d5cc0d1e2686f6900bd821e405620c0
Mithun Roy <mithunroy25@gmail.com>
2021-07-05 02:10:55 +0400

    added pinch and zoom on recyclerview

--- app/src/main/java/com/inhouse/catastrophic/ui/MainActivity.kt
+++ app/src/main/java/com/inhouse/catastrophic/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.inhouse.catastrophic.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.view.ScaleGestureDetector
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -21,8 +22,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var rvCatPhotos: RecyclerView
     private lateinit var photoAdapter: PhotoAdapter
+    private lateinit var gridLayoutManager1: GridLayoutManager
+    private lateinit var gridLayoutManager2: GridLayoutManager
+    private lateinit var gridLayoutManager3: GridLayoutManager
+    private lateinit var currentLayoutManager: RecyclerView.LayoutManager
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         mainComponent.inject(this)
         super.onCreate(savedInstanceState)
@@ -57,12 +65,25 @@ class MainActivity : AppCompatActivity() {
         binding.rvCatPhotos.removeOnScrollListener(onScrollListener)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun configurePhotoRecyclerView() {
+        rvCatPhotos = binding.rvCatPhotos
+        gridLayoutManager1 = GridLayoutManager(this, 1)
+        gridLayoutManager2 = GridLayoutManager(this, 2)
+        gridLayoutManager3 = GridLayoutManager(this, 3)
+
+        scaleGestureDetector = ScaleGestureDetector(this, ScaleListener())
+        binding.rvCatPhotos.setOnTouchListener { _, event ->
+            scaleGestureDetector.onTouchEvent(event)
+            false
+        }
+
         photoAdapter = PhotoAdapter()
-        val recyclerView = binding.rvCatPhotos
-        recyclerView.setHasFixedSize(true)
-        recyclerView.setItemViewCacheSize(20)
-        recyclerView.adapter = photoAdapter
+        currentLayoutManager = gridLayoutManager3
+        rvCatPhotos.layoutManager = gridLayoutManager3
+        rvCatPhotos.setHasFixedSize(true)
+        rvCatPhotos.setItemViewCacheSize(20)
+        rvCatPhotos.adapter = photoAdapter
     }
 
     private val onScrollListener by lazy {
@@ -76,6 +97,39 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * Gesture detector to manage grid layout column span
+     */
+    inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            if (detector.currentSpan > 200 && detector.timeDelta > 200) {
+                val pinchSpan = detector.currentSpan - detector.previousSpan
+                if (pinchSpan < -1) {
+                    if (currentLayoutManager == gridLayoutManager1) {
+                        currentLayoutManager = gridLayoutManager2
+                        rvCatPhotos.layoutManager = gridLayoutManager2
+                        return true
+                    } else if (currentLayoutManager == gridLayoutManager2) {
+                        currentLayoutManager = gridLayoutManager3
+                        rvCatPhotos.layoutManager = gridLayoutManager3
+                        return true
+                    }
+                } else if (pinchSpan > 1) {
+                    if (currentLayoutManager == gridLayoutManager3) {
+                        currentLayoutManager = gridLayoutManager2
+                        rvCatPhotos.layoutManager = gridLayoutManager2
+                        return true
+                    } else if (currentLayoutManager == gridLayoutManager2) {
+                        currentLayoutManager = gridLayoutManager1
+                        rvCatPhotos.layoutManager = gridLayoutManager1
+                        return true
+                    }
+                }
+            }
+            return false
         }
     }
 }
